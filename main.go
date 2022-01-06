1c38ca76c4207824ab43b67ccc4c88d84d1ac6e4
Omari Brown <omaris.brown@gmail.com>
2022-01-06 13:25:40 -0500

    testing cron and coinapi

--- main.go
+++ main.go
@@ -2,42 +2,49 @@ package main
 
 import (
 	"fmt"
-	"github.com/omaribrown/coinalert/slack"
+	coinapi "github.com/omaribrown/coinalert/data"
+	envVariables "github.com/omaribrown/coinalert/envvar"
 	"github.com/robfig/cron"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"net/http"
-
-	coinapi "github.com/omaribrown/coinalert/data"
-	envVariables "github.com/omaribrown/coinalert/envvar"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, world")
 	fmt.Println("starting cron job")
+	Viperenv := envVariables.ViperEnvVariable("API_KEY")
+	coinapi := &coinapi.Coinapi{
+		API_KEY: Viperenv,
+		Client:  &http.Client{},
+	}
 	c := cron.New()
-	c.AddFunc("@every 30s", func() {
-		slackService := &slack.SlackService{
-			SlackToken:     os.Getenv("SLACK_AUTH_TOKEN"),
-			SlackChannelID: os.Getenv("SLACK_CHANNEL_ID"),
-		}
+	//c.AddFunc("@every 30s", func() {
+	//	slackService := &slack.SlackService{
+	//		SlackToken:     os.Getenv("SLACK_AUTH_TOKEN"),
+	//		SlackChannelID: os.Getenv("SLACK_CHANNEL_ID"),
+	//	}
+	//
+	//	slackService.SendSlackMessage(slack.SlackMessage{
+	//		Pretext: "pre-test",
+	//		Text:    "text-test",
+	//	})
+	//})
+	c.AddFunc("@every 1m", func() {
+		ohlvcLatest := coinapi.GetCoinLatest("BTC/USD", "1MIN", "1")
+		fmt.Println("Crypto Data: ", ohlvcLatest)
 
-		slackService.SendSlackMessage(slack.SlackMessage{
-			Pretext: "pre-test",
-			Text:    "text-test",
-		})
 	})
 	c.Start()
 	select {}
 }
 func main() {
-	Viperenv := envVariables.ViperEnvVariable("API_KEY")
-	coinapi := &coinapi.Coinapi{
-		API_KEY: Viperenv,
-		Client: &http.Client{},
-	}
+	//Viperenv := envVariables.ViperEnvVariable("API_KEY")
+	//coinapi := &coinapi.Coinapi{
+	//	API_KEY: Viperenv,
+	//	Client:  &http.Client{},
+	//}
 
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", helloWorld)
@@ -46,8 +53,7 @@ func main() {
 	//Viperenv := envVariables.ViperEnvVariable("API_KEY")
 	//coinapi := &coinapi.Coinapi{API_KEY: Viperenv}
 	//
-	//ohlvcLatest := coinapi.GetCoinLatest("BTC/USD", "1DAY", "3")
-	//fmt.Println("Negative: ", ohlvcLatest)
+
 	//log.Info("Create new cron")
 
 }
