0a9efd72e607c93f5213a38fabfae5e8d2210967
Omari Brown <omaris.brown@gmail.com>
2022-01-10 15:58:41 -0500

    fixed slack message format bug

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
 	coinapi "github.com/omaribrown/coinalert/data"
@@ -10,7 +11,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"reflect"
 )
 
 func main() {
@@ -39,54 +39,41 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintln(w, string(body))
 }
-func mapToStringSlice(slice interface{}, mapFunc func(interface{}) string) []string {
-	s := reflect.ValueOf(slice)
-	if s.Kind() != reflect.Slice {
-		panic("mapToStringSlice() given a non-slice type")
-	}
-	ret := make([]string, s.Len())
 
-	for i := 0; i < s.Len(); i++ {
-		ret[i] = mapFunc(s.Index(i).Interface())
-	}
-
-	return ret
-}
 func coinToSlack(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	fmt.Fprintln(w, "Running cointoslack. Manually added a 200 Status OK.")
 
-	// * get local env's
 	envErr := godotenv.Load(".env")
 	if envErr != nil {
 		fmt.Printf("Could not load .env file")
 		os.Exit(1)
 	}
-	Viperenv := os.Getenv("API_KEY")
+
+	CoinAPIKey := os.Getenv("API_KEY")
 	coinapi := &coinapi.Coinapi{
-		API_KEY: Viperenv,
+		API_KEY: CoinAPIKey,
 		Client:  &http.Client{},
-		// * For local testing. Not validated.
 	}
-	fmt.Println(Viperenv)
+
+	slackService := &slack.SlackService{
+		SlackToken:     os.Getenv("SLACK_AUTH_TOKEN"),
+		SlackChannelID: os.Getenv("SLACK_CHANNEL_ID"),
+	}
 	c := cron.New()
-	fmt.Fprintf(w, "Starting cron job")
 	fmt.Println("starting cron job")
 
 	c.AddFunc("@every 1m", func() {
 		ohlvcLatest := coinapi.GetCoinLatest("BTC/USD", "1MIN", "1")
 
-		// * Stringify for slack
-
-		//stringData := cast.ToString(ohlvcLatest)
-		//fmt.Println(stringData)
-		//fmt.Fprintf(w, stringData)
-		fmt.Println("Crypto Data: ", ohlvcLatest)
-
-		slackService := &slack.SlackService{
-			SlackToken:     os.Getenv("SLACK_AUTH_TOKEN"),
-			SlackChannelID: os.Getenv("SLACK_CHANNEL_ID"),
+		// Marshal data for Slack
+		remarshal, err := json.Marshal(ohlvcLatest)
+		if err != nil {
+			panic(err)
 		}
+		stringData := string(remarshal)
+		fmt.Println("stringdata: ", stringData)
+
+		fmt.Println("Crypto Data: ", ohlvcLatest)
 
 		slackService.SendSlackMessage(slack.SlackMessage{
 			Pretext: "Incoming crypto data...",
